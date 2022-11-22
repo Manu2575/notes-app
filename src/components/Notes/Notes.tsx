95f74bc892562eb2a6326a08d5b561abbd253670
Manuel Duarte <manuelgarcia.duarte1@gmail.com>
2022-11-22 17:02:45 -0300

    hidden button
    Atualização feita

--- src/components/Notes/Notes.tsx
+++ src/components/Notes/Notes.tsx
@@ -106,14 +106,17 @@ export const Notes = () => {
 					<div className={classes.buttons}>
 						<SearchBar disabled={!viewCondition} title='notes' />
 						<FilterPopup disabled={!viewCondition} />
+						{notes.length > 2 ? (
 						<SecondaryButton
-							disabled={notes.length < 3}
+							disabled={notes.length < 2}
 							onClick={onViewToggle}
 							title={viewCondition ? 'Carousel view' : 'Grid view'}
 						/>
+						): null }
 						<MainButton title='Trash all' onClick={removeAll} />
+						
 					</div>
-				) : null}
+				) : null }
 			</div>
 
 			<AnimatePresence>
